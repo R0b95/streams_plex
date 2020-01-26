79c68319ba3bc3cd97db5e1845d40ecd91067aec
R0b95 <60280966+R0b95@users.noreply.github.com>
2020-01-26 21:28:04 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,2 @@
+# streams_plex
+### written in Phyton 3.8.1 by Strolch
---
+++ test.py
@@ -0,0 +1,14 @@
+# streams_plex
+### written in Phyton 3.8.1 by Strolch
+
+from streams_plex.main import main
+from streams_plex.debug import debug
+from streams_plex.setserver import  setserver
+
+#setserver()
+main()
+#debug()
+
+#print("The server is reachable, but the authentification did not work.\n"
+#      "Are you sure, that the port and plex-token are correct?\n"
+#      "Maybe it is the wrong server?")
